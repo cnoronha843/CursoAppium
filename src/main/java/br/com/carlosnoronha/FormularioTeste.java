271b2329d90b6eb656c6ae0628f830a3fc18d30a
Carlos Andre Noronha <carlosanoronha@hotmail.com>
2022-05-23 01:48:25 -0300

    Criação de classe DSL para reutilizar codigo e refatoração do codigo.

--- src/main/java/br/com/carlosnoronha/FormularioTeste.java
+++ src/main/java/br/com/carlosnoronha/FormularioTeste.java
@@ -1,5 +1,7 @@
 package br.com.carlosnoronha;
+import br.com.carlosnoronha.appium.core.DSL;
 import br.com.carlosnoronha.appium.core.DriverFactory;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -11,123 +13,86 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Locale;
 
+import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
+
 
 public class FormularioTeste {
-               public static AndroidDriver<AndroidElement> driver;
-                @Before
-                public void inicializarAppium() throws MalformedURLException, InterruptedException {
-                    driver = DriverFactory.getDiver ();
-                    //Selecionar formulario
-                    MobileElement formulario = driver.findElement (By.xpath ("//android.widget.TextView[@text= 'Formulário']"));
-                    formulario.click ();
-                    Thread.sleep (1000);
-                }
 
-                @Test
-                public void devePreencherCampo () throws InterruptedException {
-                    Thread.sleep (1000);
-
-                    //Escrever no formulario
-                    MobileElement el2 = driver.findElement (By.className ("android.widget.EditText"));
-                el2.sendKeys ("Carlos André");
-                String valorEntrada = el2.getText ();
-                //Salvar o que foi escrito
-                    MobileElement el3 = driver.findElement (By.className ("android.widget.Button"));
-                el3.click ();
-                    Thread.sleep (1000);
-
-                    //Capturar o texto escrito
-                    MobileElement el4 = driver.findElement (By.xpath ("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.TextView[2]"));
-                //Validar se o valor exibido é igual ao valor salvo
-                String valorExibido = el4.getText ();
-                Assert.assertEquals (valorEntrada,valorExibido.substring (6));
-
-            }
-                @Test
-                public void deveInteragirCombo () throws  InterruptedException {
-                    Thread.sleep (1000);
-                    //Clicar no combo
-                    MobileElement el2 = driver.findElement (By.className ("android.widget.Spinner"));
-                    el2.click ();
-                    Thread.sleep (1000);
-
-                    //Selecionar opcao desejada
-                    MobileElement el3 = driver.findElement (By.xpath ("//android.widget.CheckedTextView[@text= 'PS4']"));
-                    String opcaoSelecionada = el3.getText ().toLowerCase(Locale.ROOT);
-                    el3.click ();
-                    Thread.sleep (1000);
-
-                    //Salvar o que foi escrito
-                    MobileElement el4 = driver.findElement (By.xpath ("//android.widget.TextView[@text= 'SALVAR']"));
-                    el4.click ();
-                    Thread.sleep (1000);
-                    //Capturar o texto escrito
-                    MobileElement el5 = driver.findElement (By.xpath ("//android.widget.TextView[@text= 'Console: ps4']"));
-                    String opcaoExibida = el5.getText ();
-                    System.out.println (opcaoExibida.substring (9));
-                    Assert.assertEquals (opcaoSelecionada,opcaoExibida.substring (9));
+    private DSL dsl = new DSL ();
+    @Before
+    public void inicializarAppium() throws MalformedURLException, InterruptedException {
+        //Selecionar formulario
+        dsl.clicarFormulario ("Formulário");
+        Thread.sleep (2000);
     }
-                @Test
-                public void deveInteragirSwitchCheckBox () throws InterruptedException {
-                    //Selecionar Limpar formulario
-                    MobileElement el1 = driver.findElement (By.xpath ("//android.widget.TextView[@text= 'LIMPAR']"));
-                    el1.click ();
-                    Thread.sleep (2000);
-                    //Verificar status dos elementos
-                    MobileElement botaoSwitch = driver.findElement (By.className ("android.widget.Switch"));
-                    MobileElement checkBox = driver.findElement (By.className ("android.widget.CheckBox"));
-                    if (botaoSwitch.getAttribute ("checked").equals ("true") && checkBox.getAttribute ("checked").equals ("false")){
-            //            //Clicar nos elementos
-                        checkBox.click ();
-                    botaoSwitch.click ();
 
-            //            //Capturar o que foi alterado
-            //            //Validar se o valor exibido é igual ao valor salvo
-                        if (botaoSwitch.getAttribute ("checked").equals ("false")  && checkBox.getAttribute ("checked").equals ("true")  ){
-                            Assert.assertTrue (true);
-                        }
-                    }
-                }
-                @Test
-                public void preencherTodoFormulario () throws InterruptedException {
-                    //Selecionar Limpar formulario
-                    MobileElement el1 = driver.findElement (By.xpath ("//android.widget.TextView[@text= 'LIMPAR']"));
-                    el1.click ();
-                    MobileElement nome = driver.findElement (By.xpath ("//android.widget.EditText[@text='Nome']"));
-                    String nomeInserido = "Carlos André";
-                    nome.sendKeys (nomeInserido);
-                    MobileElement comboBox = driver.findElement (By.className ("android.widget.Spinner"));
-                    comboBox.click ();
-                    Thread.sleep (1000);
-                    MobileElement ps4 = driver.findElement (By.xpath ("//android.widget.CheckedTextView[@text='PS4']"));
-                    ps4.click ();
-                    MobileElement checkBox = driver.findElement (By.className ("android.widget.CheckBox"));
-                    checkBox.click ();
-                    MobileElement clickSwitch = driver.findElement (By.className ("android.widget.Switch"));
-                    clickSwitch.click ();
-                    MobileElement salvar = driver.findElement (By.xpath ("//android.widget.Button[@text='']"));
-                    salvar.click ();
-                    Thread.sleep (1000);
-                    String nomeExibido = driver.findElement (By.xpath ("//android.widget.TextView[starts-with(@text,'Nome:')]")).getText ().substring (6);
-                    String consoleExibido = driver.findElement (By.xpath ("//android.widget.TextView[starts-with(@text,'Console:')]")).getText ().substring (9);
-                    String switchExibido = driver.findElement (By.xpath ("//android.widget.TextView[starts-with(@text,'Switch:')]")).getText ().substring (8);
-                    String checkBoxExibido = driver.findElement (By.xpath ("//android.widget.TextView[starts-with(@text,'Checkbox:')]")).getText ().substring (10);
-                    System.out.println (nomeExibido);
-                    System.out.println (consoleExibido);
-                    System.out.println (switchExibido);
-                    System.out.println (checkBoxExibido);
-                    Assert.assertEquals (nomeInserido,nomeExibido);
-                    Assert.assertEquals ("ps4",consoleExibido);
-                    Assert.assertEquals ("Off",switchExibido);
-                    Assert.assertEquals ("Marcado",checkBoxExibido);
+    @Test
+    public void devePreencherCampo () throws InterruptedException, MalformedURLException {
+        Thread.sleep (1000);
+        //Escrever no formulario
+        String valorEntrada = "Carlos André";
+        dsl.escrever (By.className ("android.widget.EditText"),valorEntrada);
+        //Salvar o que foi escrito
+        dsl.clicar (By.className ("android.widget.Button"));
+        Thread.sleep (1000);
+        //Capturar o texto escrito
+        String valorExibido = dsl.obterTexto (MobileBy.xpath ("//android.widget.TextView[starts-with(@text,'Nome:')]")).substring (6);
+        //Validar se o valor exibido é igual ao valor salvo
+        Assert.assertEquals (valorEntrada,valorExibido);
+    }
+    @Test
+    public void deveInteragirCombo () throws InterruptedException, MalformedURLException {
+        Thread.sleep (1000);
+        //Clicar no combo
+        dsl.selecionarCombo (MobileBy.className ("android.widget.Spinner"),"PS4");
+        Thread.sleep (1000);
+//                    String opcaoSelecionada = dsl.obterTexto (MobileBy.xpath ("//android.widget.CheckedTextView[@text= 'PS4']"));
+        Thread.sleep (1000);
+        //Salvar o que foi escrito
+        dsl.clicarSalvar ();
+        Thread.sleep (1000);
+        //Capturar o texto escrito
+        String opcaoExibida = dsl.obterTexto (By.xpath ("//android.widget.TextView[@text= 'Console: ps4']"));
+        System.out.println (opcaoExibida.substring (9));
+        Assert.assertEquals ("ps4",opcaoExibida.substring (9));
+    }
+    @Test
+    public void deveInteragirSwitchCheckBox () throws InterruptedException, MalformedURLException {
+        Thread.sleep (2000);
+        Assert.assertFalse (dsl.isCheckMarcado (By.className ("android.widget.CheckBox")));
+        Assert.assertTrue (dsl.isCheckMarcado (By.className ("android.widget.Switch")));
+        Thread.sleep (1000);
+        dsl.clicar (By.className ("android.widget.CheckBox"));
+        dsl.clicar (By.className ("android.widget.Switch"));
+        Assert.assertTrue (dsl.isCheckMarcado (By.className ("android.widget.CheckBox")));
+        Assert.assertFalse (dsl.isCheckMarcado (By.className ("android.widget.Switch")));
 
+    }
+    @Test
+    public void preencherTodoFormulario () throws InterruptedException, MalformedURLException {
 
+        String valorEntrada = "Carlos André";
+        dsl.escrever (By.className ("android.widget.EditText"),valorEntrada);
+        dsl.selecionarCombo (MobileBy.className ("android.widget.Spinner"),"PS4");
+        Thread.sleep (1000);
+        dsl.clicar (By.className ("android.widget.CheckBox"));
+        dsl.clicar (By.className ("android.widget.Switch"));
+        Thread.sleep (1000);
+        dsl.clicarSalvar ();
+        Thread.sleep (1000);
+        String nomeExibido = dsl.obterTexto (By.xpath ("//android.widget.TextView[starts-with(@text,'Nome:')]")).substring (6);
+        String consoleExibido = dsl.obterTexto (By.xpath ("//android.widget.TextView[starts-with(@text,'Console:')]")).substring (9);
+        String switchExibido = dsl.obterTexto (By.xpath ("//android.widget.TextView[starts-with(@text,'Switch:')]")).substring (8);
+        String checkBoxExibido = dsl.obterTexto (By.xpath ("//android.widget.TextView[starts-with(@text,'Checkbox:')]")).substring (10);
+        Assert.assertEquals (valorEntrada,nomeExibido);
+        Assert.assertEquals ("ps4",consoleExibido);
+        Assert.assertEquals ("Off",switchExibido);
+        Assert.assertEquals ("Marcado",checkBoxExibido);
+    }
 
-                }
-
-                @After
-                public void fecharApp(){
-                    DriverFactory.killDriver ();
-                }
+    @After
+    public void fecharApp(){
+        DriverFactory.killDriver ();
+    }
 
 }
---
+++ src/main/java/br/com/carlosnoronha/appium/core/DSL.java
@@ -0,0 +1,53 @@
+package br.com.carlosnoronha.appium.core;
+
+import io.appium.java_client.MobileBy;
+import org.openqa.selenium.By;
+
+import java.net.MalformedURLException;
+
+import static br.com.carlosnoronha.appium.core.DriverFactory.*;
+
+public class DSL {
+    public void escrever (By by, String texto) throws MalformedURLException, InterruptedException {
+             getDiver().
+                     findElement (by).sendKeys (texto);
+        }
+    public String obterTexto(By by) throws MalformedURLException, InterruptedException {
+
+        return getDiver ().findElement (by).getText ();
+
+    }
+    public void selecionarCombo(By by, String valor) throws MalformedURLException, InterruptedException {
+        getDiver ().findElement (by).click ();
+//            DriverFactory.getDiver ().findElement (MobileBy.className ("android.widget.Spinner")).click ();
+        //selecionar opção desejada
+        clicarPorTexto (valor);
+    }
+
+    public void clicarFormulario(String texto) throws MalformedURLException, InterruptedException {
+        Thread.sleep (1000);
+        getDiver ().findElement (By.xpath ("//android.widget.TextView[@text= '"+texto+"']")).click ();
+
+    }
+
+    public void clicarPorTexto(String texto) throws MalformedURLException, InterruptedException {
+        Thread.sleep (1000);
+        getDiver ().findElement (By.xpath ("//android.widget.CheckedTextView[@text= '"+texto+"']")).click ();
+
+    }
+    public void clicarSalvar() throws MalformedURLException, InterruptedException {
+        Thread.sleep (1000);
+        getDiver ().findElement (By.xpath ("//android.widget.TextView[@text= 'SALVAR']")).click ();
+
+    }
+    public boolean isCheckMarcado(By by) throws MalformedURLException, InterruptedException {
+        return getDiver ().findElement (by).getAttribute ("checked").equals ("true");
+    }
+    public void clicar(By by) throws MalformedURLException, InterruptedException {
+        getDiver ().findElement (by).click ();
+    }
+
+
+
+}
+
